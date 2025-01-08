567de85204b681b107a99c5d99a4986ec1eaf5fd
A-Benai <alicebenaia@gmail.com>
2025-01-08 22:37:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-document.getElementById('copertina-sx').addEventListener('click', () => {
-  const invito = document.querySelector('.invito');
-  invito.classList.toggle('open'); // Alterna l'apertura e la chiusura
+document.getElementById('cover-right').addEventListener('click', () => {
+    // Aggiungi transizioni alla copertura sinistra e destra
+    document.getElementById('cover-left').style.transform = 'translateX(-100%)';
+    document.getElementById('cover-right').style.transform = 'translateX(100%)';
+
+    // Mostra l'immagine interna
+    setTimeout(() => {
+        document.getElementById('inner-image').style.display = 'flex';
+    }, 500); // Dopo 500ms per lasciare tempo alla transizione
 });
-
-document.getElementById('copertina-dx').addEventListener('click', () => {
-  const invito = document.querySelector('.invito');
-  invito.classList.toggle('open'); // Alterna l'apertura e la chiusura
-});
-
-
